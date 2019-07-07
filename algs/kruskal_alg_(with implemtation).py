4700c931ad41142d5b01d1c4fbc2557d5ec46e01
thxi <thxi@users.noreply.github.com>
2019-07-07 09:34:26 +0300

    kruskal small fix

--- algs/kruskal_alg_(with implemtation).py
+++ algs/kruskal_alg_(with implemtation).py
@@ -1,4 +1,5 @@
-#Graph implementation
+# Graph implementation
+from collections import defaultdict
 file = "ru_dist_mat_v1.txt"
 with open(file) as f:
     content = f.readlines()
@@ -14,87 +15,87 @@ for i in range(n):
             matrix[i][k] = float(row[k + 1])
 
 
-from collections import defaultdict
-        
-#Class to represent a graph 
-class Graph: 
-  
-    def __init__(self,vertices): 
-        self.V = vertices 
-        self.graph = [] # default dictionary to store graph 
-          
-   
-    # function to add edge
-    def addEdge(self,u,v,w): 
-        self.graph.append([u,v,w]) 
-  
-    # A utility function to find set of an element i 
-    # (uses path compression technique) 
-    def find(self, parent, i): 
-        if parent[i] == i: 
-            return i 
-        return self.find(parent, parent[i]) 
-#smth hrd
-    # A function that does union of two sets of x and y 
-    # (uses union by rank) 
-    def union(self, parent, rank, x, y): 
-        xroot = self.find(parent, x) 
-        yroot = self.find(parent, y) 
+# Class to represent a graph
+class Graph:
 
-        # Attach smaller rank tree under root of  
-        # high rank tree (Union by Rank) 
-        if rank[xroot] < rank[yroot]: 
-            parent[xroot] = yroot 
-        elif rank[xroot] > rank[yroot]: 
-            parent[yroot] = xroot 
-  
-        # If ranks are same, then make one as root  
-        # and increment its rank by one 
-        else : 
-            parent[yroot] = xroot 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []  # default dictionary to store graph
+
+    # function to add edge
+    def addEdge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    # A utility function to find set of an element i
+    # (uses path compression technique)
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+# smth hrd
+    # A function that does union of two sets of x and y
+    # (uses union by rank)
+
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        # Attach smaller rank tree under root of
+        # high rank tree (Union by Rank)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+
+        # If ranks are same, then make one as root
+        # and increment its rank by one
+        else:
+            parent[yroot] = xroot
             rank[xroot] += 1
 #
-    # The main function to construct MST using Kruskal's algorithm 
-    def KruskalMST(self): 
-  
-        result =[] #This will store the resultant MST
-        i = 0 # An index variable, used for sorted edges 
-        e = 0 # An index variable, used for result[] 
-  
-            # Sorting if all edges
-        self.graph =  sorted(self.graph,key=lambda item: item[2]) 
-  
-        parent = [] ; rank = [] 
-  
-        # Create V subsets with single elements 
-        for node in range(self.V): 
-            parent.append(node) 
-            rank.append(0) 
-      
-        # Number of edges to be taken is equal to V-1 
-        while e < self.V - 1: 
-  
-            # Construction of STree 
-            u,v,w =  self.graph[i]
+    # The main function to construct MST using Kruskal's algorithm
+    def KruskalMST(self):
+
+        result = []  # This will store the resultant MST
+        i = 0  # An index variable, used for sorted edges
+        e = 0  # An index variable, used for result[]
+
+        # Sorting if all edges
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+
+        parent = []
+        rank = []
+
+        # Create V subsets with single elements
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+
+        # Number of edges to be taken is equal to V-1
+        while e < self.V - 1:
+
+            # Construction of STree
+            u, v, w = self.graph[i]
             i = i + 1
-            x = self.find(parent, u) 
-            y = self.find(parent ,v) 
-  
-            # If including this edge does't cause cycle,  
-                        # include it in result and increment the index 
-                        # of result for next edge 
-            if x != y: 
-                e = e + 1     
-                result.append([u,v,w]) 
-                self.union(parent, rank, x, y)             
-  
-        print ("MST is:")
-        for u,v,weight  in result: 
-            print ("{} -- {} == {}".format(cities[u],cities[v],weight))
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            # If including this edge does't cause cycle,
+            # include it in result and increment the index
+            # of result for next edge
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+
+        print("MST is:")
+        for u, v, weight in result:
+            print("{} -- {} == {}".format(cities[u], cities[v], weight))
             g = Graph(n)
 
+
 for i in range(n):
-    for j in range(i + 1,n):
+    for j in range(i + 1, n):
         if matrix[i][j] != -1:
             g.addEdge(i, j, matrix[i][j])
 g.KruskalMST()
