9f23f3b06db3bde76a835554b91a96bd76257e0f
thxi <thxi@users.noreply.github.com>
2019-07-07 10:36:07 +0300

    add postfixes

--- algs/mst.py
+++ algs/mst.py
@@ -6,11 +6,14 @@ import sys
 import pandas as pd
 
 if __name__ == '__main__':
-    algs = ['boruvka', 'prim', 'krurkal']
-    file_name = 'data/ru_dist_mat_v1.csv'
-    if (len(sys.argv) == 1):
-        print(file_name)
-    mat = pd.read_csv(file_name, index_col='name')
+    file_name = 'data/ru_dist_mat'
+    postfix = 'v1'
+    if (len(sys.argv) != 1):
+        postfix = sys.argv[1]
+    postfix = '_'+postfix
+    print(file_name+postfix)
+
+    mat = pd.read_csv(file_name+postfix+'.csv', index_col='name')
 
     nodes = len(mat.columns)
     edges = utils.read_mat(mat)
@@ -20,6 +23,6 @@ if __name__ == '__main__':
     kruskal_edges = kruskal.kruskal(nodes, edges, cols)
     prim_edges = prim.prim(nodes, mat.values, cols)
 
-    utils.edges_to_csv('mst_boruvka', boruvka_edges)
-    utils.edges_to_csv('mst_kruskal', kruskal_edges)
-    utils.edges_to_csv('mst_prim', prim_edges)
+    utils.edges_to_csv('mst_boruvka'+postfix, boruvka_edges)
+    utils.edges_to_csv('mst_kruskal'+postfix, kruskal_edges)
+    utils.edges_to_csv('mst_prim'+postfix, prim_edges)
